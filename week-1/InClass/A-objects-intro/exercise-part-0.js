ccc5c005b171198f30921c487aeb83058ca6f177
linoman <2051016+linoman@users.noreply.github.com>
2021-03-17 19:59:29 +0100

    Ejercicio 0

--- week-1/InClass/A-objects-intro/exercise-part-0.js
+++ week-1/InClass/A-objects-intro/exercise-part-0.js
@@ -10,4 +10,25 @@ let laptop = {
     brand: "Lenovo",
     screenSize: 13,
     isTouchscreen: true,
+    procesador: {
+      marca: 'intel',
+      cantidadDeProcesadores: 8
+    },
+    camara: 'high vision',
+    color: 'rojo y negro',
+    memoriaRam: 8,
+    almacenamiento: ['hdd', 'ssd', 'cdrom', 'floppy', 'usb', 'microsd'],
+    puertos: ['wga', 'usb', 'hdmi', 'ethernet'],
+    so: {
+      marca: 'windows',
+      version: 'XP',
+      parche: '1.0.3'
+    },
+    encender: () => {
+      console.log('peep poop')
+    }
+
   };
+
+console.log(laptop)
+laptop.encender()
